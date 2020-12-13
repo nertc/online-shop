36f160403c68e4c5ee11cc6d6868f63087d431be
nertc <davidtsiklauri7@gmail.com>
2020-12-13 23:05:56 +0400

    feat(books): Added Book class and some books

---
+++ scripts/book.js
@@ -0,0 +1,23 @@
+export default class Book {
+    constructor( name, author, date, genre, image, price, links, description = "") {
+        this.name = name;
+        this.author = author;
+        this.date = date;
+        this.genre = genre;
+        this.image = './assets/images/books/' + image;
+        this.price = price;
+        this.links = links;
+        this.description = description;
+    }
+
+    toHTML() {
+        return `
+        <div class="book">
+            <div class="border"></div>
+            <h2>${this.name}</h2>
+            <p class="author">by ${this.author}</p>
+            <img src="${this.image}" alt="${this.name}">
+            <p class="price">$ ${this.price}</p>
+        </div>`;
+    }
+}
---
+++ scripts/genre.js
@@ -0,0 +1,8 @@
+export default Object.freeze({
+    adventure: 'adventure',
+    classics: 'classics',
+    comics: 'comics',
+    detective: 'detective',
+    fantasy: 'fantasy',
+    horror: 'horror'
+});
